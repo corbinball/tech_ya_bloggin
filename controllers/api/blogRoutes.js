d09b74e4b7b9923260d6f301d975062dbf1d4332
Corbin Ball <c.ball@me.com>
2021-11-23 00:00:32 -0600

    final fixes, hopefully

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -7,7 +7,9 @@ router.post('/', withAuth, async (req, res) => {
     const newBlog = await Blog.create({
       ...req.body,
       user_id: req.session.user_id,
-    })
+    });
+
+    res.status(200).json(newBlog);
     
   } catch (err) {
     res.status(400).json(err);
--- public/js/profile.js
+++ public/js/profile.js
@@ -13,7 +13,7 @@ const newFormHandler = async (event) => {
   if (name && contents) {
     const response = await fetch(`/api/blogs`, {
       method: 'POST',
-      body: JSON.stringify({ post_title, contents }),
+      body: JSON.stringify({ name, contents }),
       headers: {
         'Content-Type': 'application/json',
       },
